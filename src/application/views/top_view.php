532e789d8f6b05644e1a33bf3da100538d310f11
SimizuSyouta <uitaso8911@ezweb.ne.jp>
2016-10-11 18:48:38 +0900

    トップ画面の形作成

---
+++ src/application/views/top_view.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<mata http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>トップ画面</title>
+
+<body>
+<img src="http://localhost/img/12986.jpg" width="100%" height="100%"
+style="position:absolute; top:0px; left:0px; z-index:1;">
+<div style="position:absolute; top:10px; left:10px; z-index:2">
+<font size="5"color="#000000"><b>
+  Welcome to Free Space!!  
+  <?php
+  print $_POST['nickname'];
+  print '様';
+  ?>
+</b>
+</font>
+<br>
+<br>
+  <font size="7" color="#0000bb">好きなカテゴリを選択してください。
+</font>
+</body>
+</html>
