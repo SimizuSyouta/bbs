f45f60d98d64c9609fe7f87e695db060dc5b8e53
SimizuSyouta <uitaso8911@ezweb.ne.jp>
2016-10-13 18:48:21 +0900

    新規アカウント作成画面ビュー

---
+++ src/application/views/newaccount_view.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<mata http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>ログイン画面</title>
+
+<body>
+<img src="http://localhost/img/12986.jpg" width="100%" height="100%"
+style="position:absolute; top:0px; left:0px; z-index:1;">
+<div style="position:absolute; top:10px; left:10px; z-index:2">
+
+
+<font size="7"><b>Create new account</font></b>
+<form action="Newaccout/data" method="post">
+<br>
+
+
+
+<font size="4">
+<b>User ID:</b>
+<input name="username" type="text" style="width:100px">
+<br>
+<br>
+<br>
+<b>Passwd:</b>
+<input name="password" type="text" style="width:100px">
+</font>
+<br>
+<br>
+<input type="submit" value="登録する">
+
+ </form>
+
+</body>
+</html>
